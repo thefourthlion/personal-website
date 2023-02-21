1da628d1c1b650ad9d222dd8e980a091db04e774
TheFourthLion <the00110100lion@gmail.com>
2023-02-21 15:47:54 -0800

    change svg and tech

--- client/src/components/Tech/Tech.js
+++ client/src/components/Tech/Tech.js
@@ -1,8 +1,9 @@
-import { ReactComponent as Bg } from "../../styles/images/tech-bg.svg";
+import { ReactComponent as Bg } from "../../styles/images/tech-bgtech-bg2.svg";
 import "./tech.css";
 export default function Tech() {
   return (
     <div className="Tech" id="Tech">
+      {/* add class for svg-tech-names | delete weird characters | delete rect bg */}
       <Bg />
     </div>
   );
